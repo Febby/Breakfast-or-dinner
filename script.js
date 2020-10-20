a2383a55b948c991c4119f076d57fb385b5917b2
febby <gunawan.febby@gmail.com>
2020-10-20 22:36:12 +0700

    Setting up Content & Script js
    - Setting up structure & content
    - Added Theme toggle switcher function

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]')
+
+
+//Switch theme dynamically
+
+function switchTheme(event) {
+    // console.log(event.target.checked);
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+
+//Event listener
+toggleSwitch.addEventListener('change', switchTheme);
